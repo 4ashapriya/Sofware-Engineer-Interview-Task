8d963bed83e7ae9f9dd374a1d75cb3baede816dc
Ashapriya Sebastian <43288279+4ashapriya@users.noreply.github.com>
2022-12-09 09:37:57 +0000

    Update test_login.py

--- test/test_login.py
+++ test/test_login.py
@@ -1,16 +1,22 @@
 import pytest
 import requests
+import json
 
 
 url_server = "http://localhost:5000/"
 
 
-def test_login():
-    """ Test code for login """
+def test_add_data():
+    """ Test code for Upload Test data """
     url_login = url_server + "login"
-    data = {"email": "asha@mail.com",
-            "password": "password"}
+    login_data = {"email": "asha@mail.com",
+                    "password": "password"}
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    response = requests.post(url_login, data=data, headers=headers)
-    print(f"test_Login => {response}")
-    assert response.status_code == 201
+    login_response = requests.post(url_login, data=login_data, headers=headers)
+    t = json.loads(login_response.text)
+    header = {"x-access-token":t['token']}
+    url_add_data = url_server + "add_data"
+    data = 'static\Train.csv'
+    response = requests.post(url_add_data, data=data, headers=header)
+    print(f"test_AddData => {response}")
+    assert response.status_code == 200
