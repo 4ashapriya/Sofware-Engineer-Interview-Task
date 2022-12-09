339d99afc3c97b49b36a265b14ab935aaf3998e4
Ashapriya Sebastian <43288279+4ashapriya@users.noreply.github.com>
2022-12-09 09:44:52 +0000

    JWT token fixed

--- test/test_add_data.py
+++ test/test_add_data.py
@@ -1,5 +1,6 @@
 import pytest
 import requests
+import json
 
 
 url_server = "http://localhost:5000/"
@@ -7,9 +8,15 @@ url_server = "http://localhost:5000/"
 
 def test_add_data():
     """ Test code for Upload Test data """
+    url_login = url_server + "login"
+    login_data = {"email": "asha@mail.com",
+                    "password": "password"}
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    login_response = requests.post(url_login, data=login_data, headers=headers)
+    t = json.loads(login_response.text)
+    header = {"x-access-token":t['token']}
     url_add_data = url_server + "add_data"
     data = 'static\Train.csv'
-    header = {"x-access-token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwdWJsaWNfaWQiOiI4OWNlNzg3Mi1hYjcxLTRkNDAtYmNhNy0wOThjNjJkMGZkYzYiLCJleHAiOjE2NzA1Mzg0MDN9.DmJFlCk0jOlKwSmCA-q0FqtAPqqwxUQ3ZvStOEW8Rxs"}
     response = requests.post(url_add_data, data=data, headers=header)
     print(f"test_AddData => {response}")
     assert response.status_code == 200
